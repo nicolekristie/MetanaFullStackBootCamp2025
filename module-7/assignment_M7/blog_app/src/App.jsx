c693f245f6ebe02d1330697b4070d74fae49defe
nicolekristie <nkvillet@gmail.com>
2025-04-23 12:04:46 -0400

    Add updates to tailwind

---
+++ module-7/assignment_M7/blog_app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      {/* <div className='container bg-red-500'> */}
+        <div className="h-screen bg-green-400">
+        <h1> Hello tailwindcssv4 and react</h1>
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ module-7/assignment_M7/blog_app/tailwind.config.js
@@ -0,0 +1,20 @@
+// export const purge = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
+
+
+// export const darkMode = false;
+// export const theme = {
+//   extend: {},
+// };
+// export const variants = {
+//   extend: {},
+// };
+// export const plugins = [];
+// Tailwind
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+};
