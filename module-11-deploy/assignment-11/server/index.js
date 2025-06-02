f078ba494b1718fefd87840afc9e49bb39ef573c
nicolekristie <nkvillet@gmail.com>
2025-06-02 18:19:46 -0400

    add ip address to corsOption

--- module-11-deploy/assignment-11/server/index.js
+++ module-11-deploy/assignment-11/server/index.js
@@ -15,7 +15,7 @@ import usersRouter from "./routes/users.js";
 
 import cors from "cors";
 const corsOption = {
-  origin: ["http://localhost:5173", "http://localhost:5176"], //allow both ports
+  origin: ["http://localhost:5173", "http://localhost:5176", "http://54.146.154.147"], //allow both ports
 };
 
 //middleware
