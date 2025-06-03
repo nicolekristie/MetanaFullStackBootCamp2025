94028e2f03982f61ef6abb44ef5956b6580de8bd
nicolekristie <nkvillet@gmail.com>
2025-06-03 12:54:51 -0400

    updates to config

--- module-11-deploy/assignment-11/server/config.js
+++ module-11-deploy/assignment-11/server/config.js
@@ -5,3 +5,4 @@ dotenv.config();
 export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 export const USER = process.env.USER;
 export const JWT_SECRET = process.env.JWT_SECRET;
+export const VITE_API_URL = process.env.VITE_API_URL;   
